5fb169f16744f2a64534b6835c70c25bfdf71c8b
nilesh-indianic <161326016+nilesh-indianic@users.noreply.github.com>
2024-11-19 16:08:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - updated main
+          Learn React - updated main file
         </a>
       </header>
     </div>
